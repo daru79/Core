60e27a668a55717a40ea56bd5c6076c5fec6ad79
daru79 <d.wojcicki@interia.pl>
2015-07-28 14:17:49 +0200

    some changes

---
+++ prototype/modern/one_page/js/main.js
@@ -0,0 +1,15 @@
+// Sliders parameters
+$(document).ready(function(){
+    // Top slider
+    $('#top-slider').carousel({
+        interval: 3000
+    });
+    // /.Top slider
+    
+    // Promo slider
+    $('#promo-slider').carousel({
+        interval: 1500
+    });
+    // /.Promo slider
+});
+// /.Sliders parameters
