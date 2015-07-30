cd9e07ac4d1848473e980963ba5d167b4160451f
daru79 <d.wojcicki@interia.pl>
2015-07-30 21:13:45 +0200

    some important changes

--- prototype/modern/one_page/js/main.js
+++ prototype/modern/one_page/js/main.js
@@ -11,5 +11,33 @@ $(document).ready(function(){
         interval: 1500
     });
     // /.Promo slider
+    
+    mobileMenu()
 });
 // /.Sliders parameters
+
+// Change header size after scroll
+$(window).bind('scroll', function(event) {
+    if($(window).scrollTop() > 20) {
+        $('.main-header').addClass('header-short');
+        $('.main-logo img').css('max-height', 28);
+    } else {
+        $('.main-header').removeClass('header-short');
+        $('.main-logo img').css('max-height', 42);
+    }
+});
+// /.Change header size after scroll
+
+// Mobile menu
+function mobileMenu() {
+    $('#mobile-icon').bind('click', function(event) {
+        event.preventDefault(); // chodzi tutaj o wyłączenie domyślnej akcji dla (w tym wypadku) dla odnośnika
+        $('#main-nav-mobile').show(1500);
+    });
+    
+    $('#mobile-close').bind('click', function(event) {
+        event.preventDefault(); // chodzi tutaj o wyłączenie domyślnej akcji dla (w tym wypadku) dla odnośnika
+        $('#main-nav-mobile').hide(1500);
+    });
+}
+// /.Mobile menu
